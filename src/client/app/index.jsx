895a8af24adaea7599a39ba4db294f4bbe4eae1c
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2017-09-08 16:16:02 -0600

    bein' nitpicky

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -11,7 +11,7 @@ const createFbLikeButton = (d, s, id) => {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+    js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10';
     fjs.parentNode.insertBefore(js, fjs);
 };
 
@@ -21,7 +21,7 @@ const createTwitterShareButton = (d, s, id) => {
     if (d.getElementById(id)) return t;
     js = d.createElement(s);
     js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
+    js.src = 'https://platform.twitter.com/widgets.js';
     fjs.parentNode.insertBefore(js, fjs);
     t._e = [];
     t.ready = function(f) {
