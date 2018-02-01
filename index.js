5eb2154b5e74a0c0c450cc2aaa8a2c79136e0b30
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-01-31 19:44:35 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.port || 3000;
 
 app.get('*/app.min.js', (req, res, next) => {
     req.url = `${req.url}.gz`;
@@ -19,8 +20,6 @@ app.use((req, res) => {
     res.status(404);
     res.sendFile(path.join(__dirname, '/public/404.html'));
 });
-
-const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log(`react-scratchonthebreak booted up on port ${port}`);
