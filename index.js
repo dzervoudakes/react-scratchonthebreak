469ef145c6a45c03c54a748a0aba6dfebb23328e
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-02-08 18:56:45 -0700

    best practices

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
 const port = process.env.port || 3000;
 
 app.get('*/app.min.js', (req, res, next) => {
