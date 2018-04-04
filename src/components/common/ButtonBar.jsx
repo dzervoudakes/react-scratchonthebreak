03c1f88c2903564a2ae98c60285fdb45cd9d66dc
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-04-03 22:03:21 -0600

    Refactor

--- src/components/common/ButtonBar.jsx
+++ src/components/common/ButtonBar.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { RaisedButton } from 'material-ui';
 
-class ButtonBar extends React.PureComponent {
-	render() {
-		const { yes, no } = this.props;
-		return (
-			<div className="button-bar">
-				<Link to={`/${yes}`}>
-					<RaisedButton primary label="Yes" />
-				</Link>
-				<Link to={`/${no}`}>
-					<RaisedButton primary label="No" />
-				</Link>
-			</div>
-		);
-	}
-}
+const ButtonBar = ({ yes, no }) => (
+	<div className="button-bar">
+		<Link to={`/${yes}`}>
+			<RaisedButton primary label="Yes" />
+		</Link>
+		<Link to={`/${no}`}>
+			<RaisedButton primary label="No" />
+		</Link>
+	</div>
+);
 
 export default ButtonBar;
