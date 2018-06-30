1c845205aac2d2a7c3fd971dbc8a6f1d9d305d9f
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-06-30 10:35:28 -0600

    Update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,6 +7,7 @@ module.exports = {
 		'indentation': 'tab',
 		'order/properties-alphabetical-order': true,
 		'property-no-vendor-prefix': true,
+		'selector-list-comma-newline-after': 'always-multi-line',
 		'shorthand-property-no-redundant-values': true,
 		'string-quotes': 'single',
 		'value-list-comma-space-after': 'always'
